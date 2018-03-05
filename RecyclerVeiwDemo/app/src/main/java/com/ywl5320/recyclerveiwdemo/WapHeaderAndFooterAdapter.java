97dedd0ce7c2af439b4f5147b5781be8bd58b668
596353036@qq.com <ywl12345>
2018-03-05 14:27:34 +0800

    update

--- RecyclerVeiwDemo/app/src/main/java/com/ywl5320/recyclerveiwdemo/WapHeaderAndFooterAdapter.java
+++ RecyclerVeiwDemo/app/src/main/java/com/ywl5320/recyclerveiwdemo/WapHeaderAndFooterAdapter.java
@@ -6,8 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.undunion.appsdk.util.MyLog;
-
 import java.lang.reflect.Field;
 
 /**
